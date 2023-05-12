d66944be9edcbd27ba92f6c657381c69493e6593
Sabino <sabino.heitor@hotmail.com>
2023-05-12 00:22:04 -0300

    alimentando banco de dados, colocando as inserts

---
+++ insert.sql
@@ -0,0 +1,342 @@
+INSERT INTO Pessoa
+VALUES ('06980759000','Angelo Kito','M','1991-02-15','AP','Santana','Fortaleza','Travessa Rio Tipitinga','469','15');
+INSERT INTO Pessoa
+VALUES ('90097344397','Josuke Higashi Ikata','M','2000-05-22','MA','Timon','Cidade Nova','Rua Trinta e Um','65',NULL);
+INSERT INTO Pessoa
+VALUES ('71214485219','Roran Kishibe','M','1982-10-11','PI','Teresina','São João','Avenida João XXIII','11',NULL);
+INSERT INTO Pessoa
+VALUES ('95726912101','Dio Brando','M','1993-05-22','AL','Palmeira dos Índios','Jardim Brasil','Rua Vereador José Tobias de Almeida','52',NULL);
+INSERT INTO Pessoa
+VALUES ('61333524773','Jotaro Kujo','M','1980-11-12','AL','Maceió','Tabuleiro do Martins','Rua Estudante Ana Rúbia Costa Almeida','9',NULL);
+INSERT INTO Pessoa
+VALUES ('43210682151','Muhammad Avdol','M','2000-05-19','PR','Curitiba','Cidade Industrial','Avenida Juscelino Kubitschek','555',NULL);
+INSERT INTO Pessoa
+VALUES ('23370735997','Giorno Giovanna','M','2001-02-02','MA','Timon','Boa Esperança','Rua Cinco','727',NULL);
+INSERT INTO Pessoa
+VALUES ('72076438963','Bruno Bucciarati','M','1996-07-01','MA','São Luís','Cidade Olímpica','Avenida Vinte e Cinco de Dezembro','25','Apto 101');
+INSERT INTO Pessoa
+VALUES ('70490598030','Joline Kujo','F','1996-07-01','MG','Belo Horizonte','Conjunto Minascaixa','Rua N','278','Apto900');
+INSERT INTO Pessoa
+VALUES ('11041936044','Enrico Pucci','M','1996-07-01','RS','Rio Grande','Parque Residencial São Pedro','Rua Dirceu Vilar Salinas','67',NULL);
+INSERT INTO Pessoa
+VALUES ('12686571071','Hermes Costelio','M','1996-07-01','PE','Recife','Água Fria','Rua Dona Sinhá','500',NULL);
+INSERT INTO Pessoa
+VALUES ('43868643001','Jhonatan Joestar','M','1996-07-01','SC','Florianópolis','Ingleses do Rio Vermelho','Servidão Leão Marinho','926',NULL);
+INSERT INTO Pessoa
+VALUES ('81381111009','Robert Speedwagon','M','1996-07-01','DF','Brasília','Setor de Habitações Individuais Norte','Quadra SHIN QI 10 Conjunto 1','12',NULL);
+INSERT INTO Pessoa
+VALUES ('09186036092','Erina Pendleton','F','1996-07-01','PI','Teresina','Recanto das Palmeiras','Rua Cinco','818',NULL);
+INSERT INTO Pessoa
+VALUES ('77633792000','Lisa Lisa','F','1996-07-01','SE','Aracaju','17 de Março','Acesso Trinta e Sete','84',NULL);
+INSERT INTO Pessoa
+VALUES ('16143894035','Cesar Zappeli','M','1996-07-01','TO','Palmas','Jardim Taquari (Taquaralto);','Quadra T 23 Rua LO 13','25',NULL);
+INSERT INTO Pessoa
+VALUES ('16847335025','Noriaki Kakyoin','M','1996-07-01','PI','Teresina','Verde Lar','Rua Nossa Senhora das Graças','122',NULL);
+INSERT INTO Pessoa
+VALUES ('80623260042','Jean Polnareff','M','1996-07-01','MA','Timon','Parque Aliança','Rua L','888',NULL);
+INSERT INTO Pessoa
+VALUES ('67334314085','Holy Kujo','F','1996-07-01','SP','Campinas','Conjunto Habitacional Campinas F','Rua José Alves Ferreira','290',NULL);
+INSERT INTO Pessoa
+VALUES ('47422664045','Hol Horse','M','1996-07-01','AC','Rio Branco','Loteamento Joafra','Rua Belo Jardim','25',NULL);
+
+INSERT INTO Telefone
+VALUES ('31517252069','06980759000');
+INSERT INTO Telefone
+VALUES ('31675795922','06980759000');
+INSERT INTO Telefone
+VALUES ('31585113341','43868643001');
+INSERT INTO Telefone
+VALUES ('11516106780','71214485219');
+INSERT INTO Telefone
+VALUES ('11711053242','43210682151');
+INSERT INTO Telefone
+VALUES ('11662136784','23370735997');
+INSERT INTO Telefone
+VALUES ('11562566939','09186036092');
+INSERT INTO Telefone
+VALUES ('11753473760','11041936044');
+INSERT INTO Telefone
+VALUES ('31733208979','80623260042');
+INSERT INTO Telefone
+VALUES ('31835825184','77633792000');
+INSERT INTO Telefone
+VALUES ('31479089666','67334314085');
+INSERT INTO Telefone
+VALUES ('31206702131','12686571071');
+
+INSERT INTO Professor
+VALUES ('68282116','costellio@gmail.com','12686571071');
+INSERT INTO Professor
+VALUES ('29723282','jojo_joestar@gmail.com','43868643001');
+INSERT INTO Professor
+VALUES ('21236884','speedwagon@hotmail.com','81381111009');
+INSERT INTO Professor
+VALUES ('53454751','erina_pe@hotmail.com','09186036092');
+INSERT INTO Professor
+VALUES ('58524711','Lisa_lisa@gmail.com','77633792000');
+INSERT INTO Professor
+VALUES ('82415257','cesar_zappeli@hotmail.com','16143894035');
+INSERT INTO Professor
+VALUES ('54092557','kakyoin_noriaki@gmail.com','16847335025');
+INSERT INTO Professor
+VALUES ('77208283','polnareff_j@gmail.com','80623260042');
+INSERT INTO Professor
+VALUES ('91257283','holly_kuoj@gmail.com','67334314085');
+INSERT INTO Professor
+VALUES ('77502266','hol_hol_horse@gmail.com','47422664045');
+
+INSERT INTO AreaDeInteresse
+VALUES ('Banco de dados','12686571071');
+INSERT INTO AreaDeInteresse
+VALUES ('Entomologia','43868643001');
+INSERT INTO AreaDeInteresse
+VALUES ('Ciencia de dados','81381111009');
+INSERT INTO AreaDeInteresse
+VALUES ('Urologia','09186036092');
+INSERT INTO AreaDeInteresse
+VALUES ('Proctologia','77633792000');
+INSERT INTO AreaDeInteresse
+VALUES ('Calculo Numerico','16143894035');
+INSERT INTO AreaDeInteresse
+VALUES ('Paineis Fotovoltaicos','16847335025');
+INSERT INTO AreaDeInteresse
+VALUES ('Estatistica Publica','80623260042');
+INSERT INTO AreaDeInteresse
+VALUES ('Engenharia de software','67334314085');
+INSERT INTO AreaDeInteresse
+VALUES ('Edição de som','47422664045');
+
+INSERT INTO Departamento
+VALUES ('Departamento de ciencias exatas','Ciencias exatas','21 23837753','MG','Lavras','Av central','Professor Edmir Sá Santos','665');
+INSERT INTO Departamento
+VALUES ('Departamento de medicina','Medicina','21 27725598','MG','Lavras','Av central','Professor Edmir Sá Santos','922');
+INSERT INTO Departamento
+VALUES ('Departamento de ciencia da computação','Ciencia da computação e sistemas de informação','21 40028922','MG','Lavras','Av central','Professor Edmir Sá Santos','7');
+INSERT INTO Departamento
+VALUES ('Departamento de ciencia do solo','Pesquisa e desenvolvimento de solo','21 50028674','MG','Lavras','Av central','Valdemir pinheiros','78');
+INSERT INTO Departamento
+VALUES ('Departamento de biologia','Biologia','21 88098871','MG','Lavras','Av central','Valdemir pinheiros','220');
+
+INSERT INTO Curso
+VALUES ('Fisica','67369','L','N','8','40','Departamento de ciencias exatas');
+INSERT INTO Curso
+VALUES ('Matematica','22046','L','N','8','40','Departamento de ciencias exatas');
+INSERT INTO Curso
+VALUES ('Estatistica','85112','L','N','8','40','Departamento de ciencias exatas');
+INSERT INTO Curso
+VALUES ('Medicina','77317','B','I','12','30','Departamento de ciencias medicina');
+INSERT INTO Curso
+VALUES ('Enfermagem','70017','B','I','12','30','Departamento de ciencias medicina');
+INSERT INTO Curso
+VALUES ('Ciencia da computação','23591','B','I','8','30','Departamento de ciencia da computação');
+INSERT INTO Curso
+VALUES ('Sistemas de informação','23592','B','N','8','40','Departamento de ciencia da computação');
+INSERT INTO Curso
+VALUES ('Geologia','59416','B','I','8','30','Departamento de ciencia do solo');
+INSERT INTO Curso
+VALUES ('Biologia','12019','B','I','8','40','Departamento de biologia');
+INSERT INTO Curso
+VALUES ('Entomologia','12900','B','I','8','40','Departamento de biologia');
+
+INSERT INTO Disciplina
+VALUES ('10000','Calculo Numerico','120','120','4');
+INSERT INTO Disciplina
+VALUES ('11000','Calculo I','120','120','4');
+INSERT INTO Disciplina
+VALUES ('11100','Calculo II','120','120','4');
+INSERT INTO Disciplina
+VALUES ('20000','Metodologia Cientifica','100','100','2');
+INSERT INTO Disciplina
+VALUES ('30000','Estatistica experimental','120','120','4');
+INSERT INTO Disciplina
+VALUES ('44000','Farmacologia','140','140','6');
+INSERT INTO Disciplina
+VALUES ('66600','Banco de dados','10','140','2');
+INSERT INTO Disciplina
+VALUES ('66660','Arquitetura de computadores','120','120','4');
+INSERT INTO Disciplina
+VALUES ('76660','Algoritimos em Grafos','140','140','6');
+INSERT INTO Disciplina
+VALUES ('15550','Biologia para a educação','120','120','4');
+INSERT INTO Disciplina
+VALUES ('90010','Manutenção de solo','120','120','4');
+
+INSERT INTO Turma
+VALUES ('1','20221','10A','10000');
+INSERT INTO Turma
+VALUES ('2','20221','10B','10000');
+INSERT INTO Turma
+VALUES ('3','20222','10A','11000');
+INSERT INTO Turma
+VALUES ('4','20222','10B','11000');
+INSERT INTO Turma
+VALUES ('5','20222','15G','11100');
+INSERT INTO Turma
+VALUES ('6','20222','1C','20000');
+INSERT INTO Turma
+VALUES ('7','20221','1C','30000');
+INSERT INTO Turma
+VALUES ('8','20221','10C','66600');
+INSERT INTO Turma
+VALUES ('9','20211','10A','66660');
+INSERT INTO Turma
+VALUES ('10','20211','10B','66660');
+INSERT INTO Turma
+VALUES ('11','20211','10A','76660');
+INSERT INTO Turma
+VALUES ('12','20211','10B','76660');
+INSERT INTO Turma
+VALUES ('13','20212','10A','15550');
+INSERT INTO Turma
+VALUES ('14','20212','10A','90010');
+
+INSERT INTO Possui
+VALUES ('10000','22046');
+INSERT INTO Possui
+VALUES ('10000','67369');
+INSERT INTO Possui
+VALUES ('10000','85112');
+INSERT INTO Possui
+VALUES ('11000','22046');
+INSERT INTO Possui
+VALUES ('11000','67369');
+INSERT INTO Possui
+VALUES ('11000','85112');
+INSERT INTO Possui
+VALUES ('11100','22046');
+INSERT INTO Possui
+VALUES ('11100','67369');
+INSERT INTO Possui
+VALUES ('11100','85112');
+INSERT INTO Possui
+VALUES ('20000','85112');
+INSERT INTO Possui
+VALUES ('44000','77317');
+INSERT INTO Possui
+VALUES ('44000','70017');
+INSERT INTO Possui
+VALUES ('66600','23591');
+INSERT INTO Possui
+VALUES ('66600','23592');
+INSERT INTO Possui
+VALUES ('66660','23591');
+INSERT INTO Possui
+VALUES ('66660','23592');
+INSERT INTO Possui
+VALUES ('76660','23591');
+INSERT INTO Possui
+VALUES ('76660','23592');
+INSERT INTO Possui
+VALUES ('15550','12019');
+INSERT INTO Possui
+VALUES ('15550','12900');
+INSERT INTO Possui
+VALUES ('90010','59416');
+
+INSERT INTO Aluno
+VALUES ('989194900','angelo@gmail.com','2020-01-01','20201','2020-01-01','06980759000','67369');
+INSERT INTO Aluno
+VALUES ('112467032','josukeIkata@gmail.com','2020-01-01','20201','2020-01-01','90097344397','67369');
+INSERT INTO Aluno
+VALUES ('246570581','roranKishibe@hotmail.com','2020-01-01','20201','2020-01-01','71214485219','22046');
+INSERT INTO Aluno
+VALUES ('671294698','dioBrando@hotmail.com','2019-06-01','20192','2019-06-01','95726912101','22046');
+INSERT INTO Aluno
+VALUES ('918483311','jotaroKujo@hotmail.com','2019-06-01','20192','2019-06-01','61333524773','85112');
+INSERT INTO Aluno
+VALUES ('579206226','avdol@hotmail.com','2019-06-01','20192','2019-06-01','43210682151','85112');
+INSERT INTO Aluno
+VALUES ('877874918','giornoGiovanna@hotmail.com','2020-06-01','20202','2020-06-01','23370735997','77317');
+INSERT INTO Aluno
+VALUES ('822002450','buchiaratti@gmail.com','2020-06-01','20202','2020-06-01','72076438963','77317');
+INSERT INTO Aluno
+VALUES ('762494540','jolineKujo@gmail.com','2020-06-01','20202','2020-06-01','70490598030','12900');
+INSERT INTO Aluno
+VALUES ('285149055','enricoPucci@gmail.com','2020-06-01','20202','2020-06-01','11041936044','12900');
+
+INSERT INTO Inscrito
+VALUES ('06980759000','1','100');
+INSERT INTO Inscrito
+VALUES ('06980759000','2','15');
+INSERT INTO Inscrito
+VALUES ('90097344397','3','30');
+INSERT INTO Inscrito
+VALUES ('90097344397','4','80');
+INSERT INTO Inscrito
+VALUES ('71214485219','5','80');
+INSERT INTO Inscrito
+VALUES ('71214485219','6','70');
+INSERT INTO Inscrito
+VALUES ('95726912101','7','63');
+INSERT INTO Inscrito
+VALUES ('61333524773','8','10');
+INSERT INTO Inscrito
+VALUES ('43210682151','9','0');
+INSERT INTO Inscrito
+VALUES ('23370735997','10','60');
+INSERT INTO Inscrito
+VALUES ('72076438963','11','50');
+INSERT INTO Inscrito
+VALUES ('70490598030','12','100');
+INSERT INTO Inscrito
+VALUES ('11041936044','13','20');
+INSERT INTO Inscrito
+VALUES ('11041936044','14','30');
+
+INSERT INTO Leciona
+VALUES ('12686571071','1');
+INSERT INTO Leciona
+VALUES ('12686571071','2');
+INSERT INTO Leciona
+VALUES ('43868643001','3');
+INSERT INTO Leciona
+VALUES ('43868643001','4');
+INSERT INTO Leciona
+VALUES ('43868643001','5');
+INSERT INTO Leciona
+VALUES ('81381111009','6');
+INSERT INTO Leciona
+VALUES ('81381111009','7');
+INSERT INTO Leciona
+VALUES ('09186036092','8');
+INSERT INTO Leciona
+VALUES ('77633792000','9');
+INSERT INTO Leciona
+VALUES ('16143894035','10');
+INSERT INTO Leciona
+VALUES ('16847335025','11');
+INSERT INTO Leciona
+VALUES ('80623260042','12');
+INSERT INTO Leciona
+VALUES ('67334314085','13');
+INSERT INTO Leciona
+VALUES ('47422664045','14');
+
+INSERT INTO Horario
+VALUES ('Pavilhão 6','Segunda Feira 10:00','1');
+INSERT INTO Horario
+VALUES ('Pavilhão 6','Segunda Feira 13:00','2');
+INSERT INTO Horario
+VALUES ('Pavilhão 3','Terça Feira 9:00','3');
+INSERT INTO Horario
+VALUES ('Pavilhão 3','Quarta Feira 11:00','4');
+INSERT INTO Horario
+VALUES ('Pavilhão 2','Quinta Feira 15:00','5');
+INSERT INTO Horario
+VALUES ('Pavilhão 1','Sexta Feira 13:00','6');
+INSERT INTO Horario
+VALUES ('Pavilhão 1','Segunda Feira 13:00','7');
+INSERT INTO Horario
+VALUES ('Laboratorio 6','Terça Feira 16:00','8');
+INSERT INTO Horario
+VALUES ('Laboratorio 3','Segunda Feira 9:00','9');
+INSERT INTO Horario
+VALUES ('Laboratorio 3','Quinta Feira 7:00','10');
+INSERT INTO Horario
+VALUES ('Laboratorio 6','Segunda Feira 7:00','11');
+INSERT INTO Horario
+VALUES ('Pavilhão 2','Sexta Feira 14:00','12');
+INSERT INTO Horario
+VALUES ('Pavilhão 1','Sexta Feira 15:00','13');
+INSERT INTO Horario
+VALUES ('Pavilhão 1','Quarta Feira 20:00','14');
